8d6476cad50b32370dcce2ad49a4b1f8811da0a8
John Dwyer <jgdwyer@gmail.com>
2017-03-19 14:42:05 -0700

    updated to work with updated calc_sgp

--- run.py
+++ run.py
@@ -1,12 +1,29 @@
-import calc_sgp as score
+import calc_sgp
+import addcats
+import calc_sgp_P as scorep
+import pandas as pd
 
 def do_hitters():
-    sgp_addends = [0, 0, 0, 0, 0, 0, 0, 0]
-    for i in range(10):
-        score.sgp_hitters(sgp_addends)
-        score.addpos()
-        sgp_addends, sgp_pos_addends = score.calc_pos_scarcity(sgp_addends)
-        print('Loop {}'.format(i))
+    df = addcats.add_hitters()
+    # sgp_addends = [0, 0, 0, 0, 0, 0, 0, 0]
+    sgp_addends = pd.DataFrame(data={'sAVG': 0, 'sOBP': 0, 'sSLG': 0, 'sHR': 0,
+                                     'sR': 0, 'sRBI': 0, 'sSB': 0, 'sTB': 0}, index=[0])
+
+    for i in range(5):
+        df = calc_sgp.sgp_hitters(df, sgp_addends)
+        meta = calc_sgp.addpos(df)
+        sgp_addends, sgp_pos_addends, meta_ranked = calc_sgp.calc_pos_scarcity(sgp_addends, meta)
+        print('Loop {:d}'.format(i))
         print(sgp_addends)
         print(sgp_pos_addends)
-    score.add_pos_sgp(sgp_pos_addends)
+    return meta_ranked
+    # calc_sgp.add_pos_sgp(sgp_pos_addends)
+
+
+def do_pitchers():
+    sgp_addends = [0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(10):
+        scorep.calc_sgp_SPRP(sgp_addends)
+        sgp_addends, sgp_thresh = scorep.normalize_SPRP(sgp_addends)
+        print(sgp_thresh)
+    scorep.reorder_cols()
