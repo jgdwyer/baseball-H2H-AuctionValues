4a92429e192004eccca273550ec290137357a05e
John Dwyer <jgdwyer@gmail.com>
2017-03-22 21:56:55 -0700

    Added new script to write to an xlsx file

--- run.py
+++ run.py
@@ -10,22 +10,35 @@ def do_hitters():
 
     for i in range(5):
         df = calc_sgp.sgp_hitters(df, sgp_addends)
-        meta = calc_sgp.addpos(df)
+        meta = calc_sgp.addcbs_info(df, 'hitters')
         sgp_addends, sgp_pos_addends, meta_ranked = calc_sgp.calc_pos_scarcity(sgp_addends, meta)
         print('Loop {:d}'.format(i))
         print(sgp_addends)
         print(sgp_pos_addends)
-    out = calc_sgp.add_pos_sgp(meta['U'], sgp_pos_addends)
-    return out
+    U, meta = calc_sgp.add_pos_sgp(meta['U'], sgp_pos_addends)
+    return U, meta
 
 
 def do_pitchers():
     df = addcats.add_pitchers()
+    df = calc_sgp.addcbs_info(df, 'pitchers')
     SP, RP, SPRP = addcats.separate_SP_RP(df)
     sgp_addends = pd.DataFrame(data={'sERA': 0, 'sWHIP': 0, 'sIP/GS': 0, 'sSO/BB': 0,
                                      'sSO': 0, 'sW': 0, 'sSV': 0, 'sHLD': 0}, index=[0])
     for i in range(10):
-        SP, RP = calc_sgp.calc_sgp_SPRP(sgp_addends, SP, RP, SPRP)
+        SP, RP, P = calc_sgp.calc_sgp_SPRP(sgp_addends, SP, RP, SPRP)
         sgp_addends, sgp_thresh = calc_sgp.normalize_SPRP(sgp_addends, SP, RP)
         print(sgp_thresh)
-    # scorep.reorder_cols()
+    P, SP, RP = calc_sgp.reorder_cols(P)# scorep.reorder_cols()
+    return P, SP, RP
+
+def write_to_file():
+    U, meta = do_hitters()
+    P, SP, RP = do_pitchers()
+    writer = pd.ExcelWriter('jabo_3-21.xlsx', engine='xlsxwriter')
+    U.to_excel(writer, sheet_name='U')
+    SP.to_excel(writer, sheet_name='SP')
+    RP.to_excel(writer, sheet_name='RP')
+    for key, _ in meta.items():
+        meta[key].to_excel(writer, sheet_name=key)
+    writer.save()
